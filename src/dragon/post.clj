b436446c09522e2c2438ca24fc7cc3bb09f4411c
Duncan McGreggor <oubiwann@gmail.com>
2017-03-09 00:03:47 -0600

    Added post functions.

--- src/dragon/post.clj
+++ src/dragon/post.clj
@@ -1,1 +1,18 @@
-(ns dragon.post)
+(ns dragon.post
+  (:require [clojure.string :as string]
+            [dragon.util :as util]))
+
+(defn get-all
+  []
+  (util/get-files "posts"))
+
+(defn ->dst-path
+  [src-path]
+  (-> src-path
+      (string/replace-first "posts" "docs")))
+
+;; get new names:
+;; (map (comp (fn [x] (str x "/title.html")) post/->dst-path (fn [x] (.getParent x))) (post/get-all))
+
+;; format dates:
+;; (map (comp util/format-date util/path->date (fn [x] (.getPath x))) (post/get-all))
