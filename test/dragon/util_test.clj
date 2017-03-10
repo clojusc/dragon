8b38cdc48f1e7897591e0a2d6bd1b2ec1b75a12a
Duncan McGreggor <oubiwann@gmail.com>
2017-03-09 23:43:19 -0600

    Added unit tests.

--- test/dragon/util_test.clj
+++ test/dragon/util_test.clj
@@ -4,4 +4,11 @@
             [dragon.util :as util]))
 
 (deftest path->date
-  (is (= (util/path->date "posts/2017-01/07-050909/content.rfc5322"))))
+  (is (= (util/path->date "posts/2017-01/07-050909/content.rfc5322")
+         {:year "2017" :month "01" :day "07"
+          :hour "05" :minute "09" :second "09"})))
+
+(deftest format-date
+  (is (= (util/format-date {:year "2017" :month "01" :day "07"
+                            :hour "05" :minute "09" :second "09"})
+         "2017-01-07 05:09:09")))
