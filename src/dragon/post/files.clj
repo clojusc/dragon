9c132efc2f8dbfadb5ce05b2567d2ea217a83f6a
Duncan McGreggor <oubiwann@gmail.com>
2017-03-09 23:49:28 -0600

    Moved functions into dedicated namespaces.

---
+++ src/dragon/post/files.clj
@@ -0,0 +1,15 @@
+(ns dragon.post.files
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [dragon.util :as util]))
+
+(defn process
+  ""
+  [data]
+  ;; XXX write post to archives
+  ;; XXX update archives year page with link
+  ;; XXX update category page with link
+  ;; XXX update tags page(s) with link
+  ;; XXX update authors page with link
+  ;; XXX update front page
+  data)
