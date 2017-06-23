da23d709e1446ae620cdeca51d67279cfbfb7bc1
Duncan McGreggor <oubiwann@gmail.com>
2017-06-22 19:35:37 -0500

    Support a docroot.

--- src/dragon/web.clj
+++ src/dragon/web.clj
@@ -6,11 +6,13 @@
             [taoensso.timbre :as log]))
 
 (defn run
-  [stasis-routes port]
-  (log/infof
-    (str "Starting development HTTP server on port %s using dynamic "
-         "content ...")
-    port)
-  (server/run-server
-    (ring-file/wrap-file {} "docs")
-    {:port port}))
+  ([stasis-routes port]
+    (run stasis-routes port "docs"))
+  ([stasis-routes port docroot]
+    (log/infof
+      (str "Starting development HTTP server on port %s using dynamic "
+           "content ...")
+      port)
+    (server/run-server
+      (ring-file/wrap-file {} docroot)
+      {:port port})))
