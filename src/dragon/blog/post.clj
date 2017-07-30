6ce6051ea2364b421c643b4e3441824d48450560
Duncan McGreggor <oubiwann@gmail.com>
2017-07-30 14:19:07 -0500

    Better excerpt and tag support.

--- src/dragon/blog/post.clj
+++ src/dragon/blog/post.clj
@@ -5,14 +5,37 @@
             [dragon.util :as util]
             [markdown.core :as markdown]))
 
-(defn update-tags
-  [data]
-  (assoc data :tags (apply sorted-set (string/split (:tags data) #",\s?"))))
+(defn md->html
+  [md]
+  (markdown/md-to-html-string md :inhibit-separator "%%%"))
+
+(defn join-excerpt
+  [words number]
+  (let [excerpt (string/join " " (take number words))]
+    (if (string/ends-with? excerpt ".")
+      (str excerpt "..")
+      (str excerpt "..."))))
 
 (defn convert-body
   [data]
-  (case (keyword (:content-type data))
-    :md (update-in data [:body] markdown/md-to-html-string)))
+  (let [paragraphs (string/split (:body data) #"\n\n")
+        words (-> paragraphs
+                      (first)
+                      (string/split #"\s"))
+        words-100 (take 100 words)
+        excerpt-100 (join-excerpt words-100 100)
+        excerpt-50 (join-excerpt words-100 50)
+        excerpt-25 (join-excerpt words-100 25)]
+    (case (keyword (:content-type data))
+      :md (-> data
+              (update-in [:body] md->html)
+              (assoc :excerpt-100 (md->html excerpt-100)
+                     :excerpt-50 (md->html excerpt-50)
+                     :excerpt-25 (md->html excerpt-25))))))
+
+(defn update-tags
+  [data]
+  (assoc data :tags (apply sorted-set (string/split (:tags data) #",\s?"))))
 
 (defn add-file-data
   [data]
