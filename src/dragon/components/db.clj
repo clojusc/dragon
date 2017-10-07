fb3a58eca86a14d1fbf23cff2a9537a878a2d691
Duncan McGreggor <oubiwann@gmail.com>
2017-10-07 01:58:44 -0500

    Add schema when db component starts up.

--- src/dragon/components/db.clj
+++ src/dragon/components/db.clj
@@ -6,10 +6,15 @@
             [datomic.client :as datomic]
             [dragon.components.core :as component-api]
             [dragon.config :as config]
+            [dragon.data.core :as data]
             [dragon.event.subscription :as subscription]
             [dragon.event.tag :as tag]
             [dragon.event.topic :as topic]
             [taoensso.timbre :as log]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Support / Utility Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn execute-db-command!
   [component]
@@ -31,6 +36,21 @@
         result)))
   (Thread/sleep (config/db-start-delay component)))
 
+(defn setup-schemas
+  [component]
+  ;; XXX Add check to see if schema already exists ... maybe? Or compact the db
+  ;;     after it's been run many times?
+  (log/trace (-> component
+                 :conn
+                 (data/add-schemas)
+                 vec)))
+
+(defn run-setup-tasks
+  [component]
+  (log/debug "Starting setup tasks ...")
+  (setup-schemas component)
+  (log/debug "Finished setup tasks."))
+
 (defn stop-db!
   [component]
   ;; XXX If we ever support more than one type of database, we can just do a
@@ -45,22 +65,34 @@
        (concat ["xargs" "kill"])
        (apply shell/sh)))
 
+(defn add-conn
+  [component]
+  ;; XXX If we ever support more than one type of database, we can just do a
+  ;;     case construct here ...
+  (let [cfg (config/db-config component)
+        conn (async/<!! (datomic/connect cfg))]
+    (log/debug "Using configuration:" cfg)
+    (if (:cognitect.anomalies/category conn)
+      (do
+        (log/error (:cognitect.anomalies/message conn))
+        component)
+      (do
+        (log/debug "Started db component.")
+        (assoc component :conn conn)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Components   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defrecord DataBase []
   component/Lifecycle
 
   (start [component]
     (log/info "Starting db component ...")
     (start-db! component)
-    (let [cfg (config/db-config component)
-          conn (async/<!! (datomic/connect cfg))]
-      (log/debug "Using configuration:" cfg)
-      (if (:cognitect.anomalies/category conn)
-        (do
-          (log/error (:cognitect.anomalies/message conn))
-          component)
-        (do
-          (log/debug "Started db component.")
-          (assoc component :conn conn)))))
+    (let [component (add-conn component)]
+      (run-setup-tasks component)
+      component))
 
   (stop [component]
     (log/info "Stopping db component ...")
