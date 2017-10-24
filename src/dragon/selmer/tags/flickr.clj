65d8d9fc8d4014ceb0eee4d3af3c91baf481a968
Duncan McGreggor <oubiwann@gmail.com>
2017-10-23 23:04:48 -0500

    Added implementation details for flickr-img.

--- src/dragon/selmer/tags/flickr.clj
+++ src/dragon/selmer/tags/flickr.clj
@@ -1,8 +1,31 @@
 (ns dragon.selmer.tags.flickr
   (:require [clojure.string :as string]
+            [clojure.walk :as walk]
             [taoensso.timbre :as log]
             [trifl.core :as util]))
 
-(defn img-tag [args context-map]
-  (log/warn "Passed args:" args)
-  (log/warn "Passed context-map:" context-map))
+(def str-keys->kwd-keys
+  {":photo-id" :photo-id
+   ":album-id" :album-id
+   ":height" :height
+   ":width" :width})
+
+(defn args->map
+  [args]
+  (->> args
+       (partition 2)
+       (map vec)
+       (into {})
+       (walk/postwalk-replace str-keys->kwd-keys)))
+
+(defn make-api-call
+  []
+  )
+
+(defn img-tag [raw-args context-map]
+  (let [args (args->map raw-args)]
+    (log/warn "Parsed args:" args)
+    (format (str "<a href=\"\">"
+                 "<img src=\"https://www.flickr.com/photos/forgottenroadsmx/%s/in/%s\">")
+            (:photo-id args)
+            (:album-id args))))
