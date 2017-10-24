e667315609b24a95a08409fb95f84b757a73fa2a
Duncan McGreggor <oubiwann@gmail.com>
2017-10-23 21:43:32 -0500

    Allow the content type to be multi-valued.

--- src/dragon/blog/content/core.clj
+++ src/dragon/blog/content/core.clj
@@ -1,18 +1,33 @@
 (ns dragon.blog.content.core
-  (:require [dragon.blog.content.rfc5322 :as rfc5322]
+  (:require [clojure.string :as string]
+            [dragon.blog.content.rfc5322 :as rfc5322]
             [dragon.event.system.core :as event]
             [dragon.event.tag :as tag]
+            [taoensso.timbre :as log]
             [trifl.fs :as fs]))
+
+(defn parse-content-type
+  [content-type]
+  (let [types (string/split content-type #",")
+        parts (count types)]
+    (if (= 1 parts)
+      (keyword content-type)
+      (map (comp keyword string/trim) types))))
+
+(defn file-extension-dispatch
+  [system content extension]
+  (case extension
+        :rfc5322 (rfc5322/parse system content)
+        :default {:raw-data content
+                  :error :parser-not-found}))
 
 (defn parse
   [system file-obj]
   (event/publish system tag/read-source-pre {:file file-obj})
-  (let [file-type (fs/extension file-obj)
+  (let [extension (fs/extension file-obj)
         content (slurp file-obj)]
     (event/publish system tag/read-source-post {:file file-obj})
-    (assoc
-      (case file-type
-        :rfc5322 (rfc5322/parse system content)
-        :default {:raw-data content
-                  :error :parser-not-found})
-     :file-type file-type)))
+    (-> extension
+        ((partial file-extension-dispatch system content))
+        (update-in [:content-type] parse-content-type)
+        (assoc :file-type extension))))
