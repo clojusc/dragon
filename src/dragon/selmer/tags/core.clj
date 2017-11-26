3f1731a99a498616755a48786a6d0de8a616789d
Duncan McGreggor <oubiwann@gmail.com>
2017-11-26 02:53:09 -0600

    Added new selmer tags for generating trip post legend.

--- src/dragon/selmer/tags/core.clj
+++ src/dragon/selmer/tags/core.clj
@@ -1,10 +1,16 @@
 (ns dragon.selmer.tags.core
   (:require [dragon.selmer.tags.demo :as demo]
             [dragon.selmer.tags.flickr :as flickr]
+            [dragon.selmer.tags.panel :as panel]
             [selmer.parser :as parser]
             [taoensso.timbre :as log]))
 
 (defn register!
   []
   (parser/add-tag! :flickr-img flickr/img-tag)
-  (parser/add-tag! :foo demo/foo-tag))
+  (parser/add-tag! :foo demo/foo-tag)
+  (parser/add-tag! :panel panel/tag :end-panel)
+  (parser/add-tag! :panel-body panel/body-tag :end-panel-body)
+  (parser/add-tag! :panel-table panel/table-tag :end-panel-table)
+  (parser/add-tag! :panel-row panel/row-tag :end-panel-row)
+  (parser/add-tag! :panel-img panel/img-tag))
