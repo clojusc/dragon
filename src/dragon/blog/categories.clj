5826487e9594f4c28077329bc2c42c8c424adef7
Duncan McGreggor <oubiwann@gmail.com>
2018-01-21 23:02:45 -0600

    Added category stats.

---
+++ src/dragon/blog/categories.clj
@@ -0,0 +1,42 @@
+(ns dragon.blog.categories)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Utility Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn unique
+  [data]
+  (set (map :category data)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Core Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn get-freqs
+  [data]
+  (frequencies (map :category data)))
+
+(defn add-percents
+  [total highest category-data]
+  (let [percent (float (/ (:count category-data) highest))]
+    (assoc category-data
+           :total total
+           :highest highest
+           :percent percent
+           :five-star (Math/round (* 5 percent))
+           :hundred (Math/round (* 100 percent)))))
+
+(defn add-all-percents
+  [total highest categorys-data]
+  (map #(add-percents total highest %) categorys-data))
+
+(defn get-stats
+  [data]
+  (let [freqs (get-freqs data)
+        total (reduce + (vals freqs))
+        highest (apply max (vals freqs))]
+    (->> freqs
+         vec
+         (map #(zipmap [:name :count] %))
+         (add-all-percents total highest)
+         (sort-by :name))))
